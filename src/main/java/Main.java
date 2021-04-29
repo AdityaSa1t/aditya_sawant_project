66aec6b43d56a08b1332b0cf145910377d2b8f9c
kewalshah <kshah205@uic.edu>
2021-04-29 11:55:25 -0500

    Temp driver - To be removed.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,19 @@
-import com.cs514.rbatch.utility.RedisClientUtil;
-import org.redisson.api.RList;
-import org.redisson.api.RedissonClient;
+import com.cs514.rbatch.api.RBatchProcessor;
+
+import java.lang.reflect.InvocationTargetException;
 
 public class Main {
 
-    public static void main(String[] args) {
-        RedissonClient redissonClient = RedisClientUtil.redissonClient;
-        RList<Object> q1 = redissonClient.getList("q1");
-        q1.add("Test");
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, ClassNotFoundException {
+        TestObj t1 = new TestObj(1, "test");
+        TestObj t2 = new TestObj(2, "test");
+        TestObj t3 = new TestObj(3, "test");
+        TestObj t4 = new TestObj(4, "test");
+        TestCallback t = new TestCallback();
+        String qName = "q10";
+        RBatchProcessor.enqueue(qName, t1, t);
+        RBatchProcessor.enqueue(qName, t2, t);
+        RBatchProcessor.enqueue(qName, t3, t);
+        RBatchProcessor.enqueue(qName, t4, t);
     }
 }
