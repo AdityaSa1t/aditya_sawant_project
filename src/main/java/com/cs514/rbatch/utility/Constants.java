a6b112e8480feaa77253a855559cd9d17b8305dc
kewalshah <kshah205@uic.edu>
2021-04-29 11:54:59 -0500

    Initial commit for Constants

---
+++ src/main/java/com/cs514/rbatch/utility/Constants.java
@@ -0,0 +1,11 @@
+package com.cs514.rbatch.utility;
+
+public final class Constants {
+
+    public static final String REDIS_CONFIG_FILE_NAME = "redis_config.yml";
+    public static final String RBATCH_CONFIG_FILE_NAME = "rbatch_config.yml";
+
+    public static final String SIZE = "size";
+    public static final String CLAZZ = "clazz";
+    public static final String CALLBACK = "callback";
+}
