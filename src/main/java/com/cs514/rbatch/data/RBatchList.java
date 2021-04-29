a30800517322917e6e0e0a9d67398dc12aae8a03
kewalshah <kshah205@uic.edu>
2021-04-29 11:56:22 -0500

    Refactored code.

---
+++ src/main/java/com/cs514/rbatch/data/RBatchList.java
@@ -0,0 +1,73 @@
+package com.cs514.rbatch.data;
+
+import com.cs514.rbatch.utility.RedisClientUtil;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
+public class RBatchList {
+
+    private String name;
+    private Class clazz;
+    private Object caller;
+    private Method callBack;
+    private boolean isBounded;
+    private long size;
+
+    private RBatchList(){
+
+    }
+
+    public boolean add(Object obj) throws InvocationTargetException, IllegalAccessException {
+        if(obj.getClass() == clazz){
+            RedisClientUtil.addToList(this.name, obj);
+            if(isBounded && RedisClientUtil.getListSize(name)==size)
+                makeCallback();
+            return true;
+        }
+        throw new IllegalArgumentException("List "+name+" does not accepts only objects of type "+clazz.getName());
+    }
+
+    private void makeCallback() throws InvocationTargetException, IllegalAccessException {
+        callBack.invoke(caller, RedisClientUtil.getList(name));
+        RedisClientUtil.clearList(name);
+    }
+
+    public static class RBatchListBuilder {
+        private String name;
+        private Class clazz;
+        private Object caller;
+        private Method callback;
+        private boolean isBounded;
+        private long size;
+
+        public RBatchListBuilder(String name, Class clazz, Object caller, Method callback){
+            this.name = name;
+            this.clazz = clazz;
+            this.isBounded = false;
+            this.caller = caller;
+            this.callback = callback;
+        }
+        
+        public RBatchListBuilder size(Integer size){
+            if(size!=null) {
+                this.size = size;
+                this.isBounded = true;
+            }
+            return this;
+        }
+
+        public RBatchList build(){
+            RBatchList queue = new RBatchList();
+            queue.name = this.name;
+            queue.clazz = clazz;
+            queue.isBounded = this.isBounded;
+            queue.size = this.size;
+            queue.caller = this.caller;
+            queue.callBack = this.callback;
+            return queue;
+        }
+        
+    }
+}
