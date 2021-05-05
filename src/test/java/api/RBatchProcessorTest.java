aff51862366ae644b84ad3178dab0f3288a691e0
kewalshah <kshah205@uic.edu>
2021-05-05 15:49:12 -0500

    Removed junit.jupiter dependency.

--- src/test/java/api/RBatchProcessorTest.java
+++ src/test/java/api/RBatchProcessorTest.java
@@ -5,15 +5,11 @@ import com.cs540.rbatch.driver.TestCallback;
 import com.cs540.rbatch.driver.TestObj;
 import com.cs540.rbatch.utility.RedisClientUtil;
 import org.junit.Test;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.TestMethodOrder;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
 public class RBatchProcessorTest {
 
     @Test
-    @DisplayName("Adds an object to a list")
     public void addOneObject() throws NoSuchMethodException, ClassNotFoundException {
         TestObj t = new TestObj(1, "Test-1");
         RBatchProcessor.enqueue("qA1", t, new TestCallback());
@@ -22,7 +18,6 @@ public class RBatchProcessorTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    @DisplayName("Adds an object to a list that does not exist")
     public void addOneObjectToListThatDoesNotExist() throws NoSuchMethodException, ClassNotFoundException {
         TestObj t = new TestObj(1, "Test-1");
         RBatchProcessor.enqueue("qA11", t, new TestCallback());
